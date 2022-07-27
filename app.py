4193056cb90f3bdb93ccaad83348110e71f11ff8
YuriDLL <fifnarr@gmail.com>
2022-07-28 01:54:17 +0300

    add: resources

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from src.game_info import get_cards, get_recipes
+from src.game_info import Cards, get_recipes
 
 app = Flask(__name__)
 
@@ -7,24 +7,24 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/recipes')
 def main_page():
-    cards = get_cards()
+    cards = Cards()
     cards_by_category = {}
-    for card in cards:
-        category = cards[card]['category']
+    for name_key, info in cards.iterate():
+        category = info['category']
         if category in cards_by_category:
-            cards_by_category[category].append(cards[card])
+            cards_by_category[category].append(info)
         else:
-            cards_by_category[category] = [cards[card]]
+            cards_by_category[category] = [info]
     return render_template('main_page.jinja', cards=cards_by_category)
 
 
 @app.route('/recipes/<name>')
 def recipes(name=None):
-    cards = get_cards()
-    create_recipes = get_recipes(output=name)
+    cards = Cards()
+    create_recipes = get_recipes(cards, output=name)
     return render_template(
         'card_templ.jinja',
-        card=cards[name],
+        card=cards.get_card(name),
         create_recipes=create_recipes
     )
 
--- src/game_info.py
+++ src/game_info.py
@@ -1,15 +1,48 @@
 import csv
-from enum import Enum, auto
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Any, Dict, List
 
 from flask import url_for
 
 
-class Categories(Enum):
-    Resources = auto()
-    Enemies = auto()
-    People = auto()
+class Cards:
+
+    def __init__(self) -> None:
+        self._prepare_cards()
+
+    def get_card(self, name_key: str) -> Dict[str, Any]:
+        return self.cards[name_key]
+
+    def iterate(self):
+        return self.cards.items()
+
+    def _prepare_cards(self) -> None:
+        self.cards: Dict[str, Dict[str, Any]] = {}
+        with open('data/boosters.csv', 'r') as csv_file:
+            boosters = csv.DictReader(csv_file)
+            for card in boosters:
+                name_key = self._get_url_name(card['name'])
+                self.cards[name_key] = card
+                self.cards[name_key]['category'] = 'Boosters'
+                self.cards[name_key]['url'] = '/recipes/'+name_key
+                self.cards[name_key]['png_url'] = url_for(
+                    'static',
+                    filename=f"img/{card['image_name']}.png"
+                )
+        with open('data/resources.csv', 'r') as csv_file:
+            resources = csv.DictReader(csv_file)
+            for card in resources:
+                name_key = self._get_url_name(card['name'])
+                self.cards[name_key] = card
+                self.cards[name_key]['category'] = 'Resources'
+                self.cards[name_key]['url'] = f'/recipes/{name_key}'
+                self.cards[name_key]['png_url'] = url_for(
+                    'static',
+                    filename=f'img/{name_key}.png'
+                )
+
+    def _get_url_name(self, name: str) -> str:
+        return name.lower().replace(' ', '_')
 
 
 @dataclass
@@ -19,99 +52,39 @@ class Recipe:
     time: int  # sec
     place: str
 
-    def to_dict(self) -> Dict:
+    def to_dict(self, cards: Cards) -> Dict:
         return {
-            'inputs': [_card_info(card) | {'count': count}
+            'inputs': [cards.get_card(card) | {'count': count}
                        for card, count in self.inputs.items()],
-            'outputs': [_card_info(card) | {'count': count}
+            'outputs': [cards.get_card(card) | {'count': count}
                         for card, count in self.outputs.items()],
             'time': self.time,
             'place': self.place
         }
 
 
-cards_tem = {
-    'Wood': Categories.Resources,
-    'Stone': Categories.Resources,
-    'Bone': Categories.Resources,
-    'Stick': Categories.Resources,
-
-    'Villager': Categories.People,
-
-    'Wolf': Categories.Enemies
-}
-
-
-def get_categories_names() -> List[str]:
-    return [name.name for name in Categories]
-
-
-def get_cards() -> Dict[str, dict]:
-    cards: dict = {}
-    with open('data/boosters.csv', 'r') as csv_file:
-        boosters = csv.DictReader(csv_file)
-        for booster in boosters:
-            card_url_name = _get_url_name(booster['name'])
-            cards |= {
-                card_url_name: booster | {
-                    'category': 'Boosters',
-                    'url': '/recipes/'+card_url_name,
-                    'png_url': url_for('static', filename=booster['image_name'] + '.png')
-                    }
-            }
-    return cards | {
-        _get_url_name(card): _card_info(card)
-        for card in cards_tem
-    }
-
-
-def _card_info(name: str) -> dict:
-    card_url = '/recipes/' + _get_url_name(name)
-    card_png_url = url_for('static', filename=_get_url_name(name) + '.png')
-    return {
-        'name': name,
-        'url': card_url,
-        'png_url': card_png_url,
-        'category': cards_tem[name].name
-    }
-
-
-def _get_url_name(name: str) -> str:
-    return name.lower().replace(' ', '_')
-
-
-def _get_name(url_name: str) -> str:
-    splits = url_name.split('_')
-    name = ''
-    for split in splits:
-        name += split[0].upper() + split[1:] + ' '
-    name = name[:-1]
-    return name
-
-
-def get_recipes(input: str = None, output: str = None) -> List[dict]:
-    recipes = []
+def get_recipes(
+        cards: Cards,
+        input: str = None,
+        output: str = None) -> List[dict]:
     if input:
-        input = _get_name(input)
-        recipes = [recipe.to_dict() for recipe in recipes_list
+        recipes = [recipe.to_dict(cards) for recipe in recipes_list
                    if input in recipe.inputs]
     elif output:
-        output = _get_name(output)
-        print(output)
-        recipes = [recipe.to_dict() for recipe in recipes_list
+        recipes = [recipe.to_dict(cards) for recipe in recipes_list
                    if output in recipe.outputs]
     return recipes
 
 
 recipes_list = [
     Recipe(
-        inputs={'Villager': 1, 'Wood': 2},
-        outputs={'Stick': 1},
+        inputs={'stone': 1, 'wood': 2},
+        outputs={'stick': 1},
         time=30,
         place='Desk'),
     Recipe(
-        inputs={'Wood': 3},
-        outputs={'Stick': 2},
+        inputs={'wood': 3},
+        outputs={'stick': 2},
         time=10,
         place='Desk')
 ]
